766801f09a28409f03a47472f0de3112c68319f0
Jaemyeong Jin <jaemyeong@me.com>
2022-04-23 16:17:04 +0900

    3.14.2 지원

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NMapsMap",
-            url: "https://github.com/jaemyeong/NMapsMap-SPM/releases/download/3.14.1/NMapsMap.xcframework.zip",
-            checksum: "400bf1dcacdfa9ac3c41dac7e60e05b149903ca9cc0195a4e7a3dc100472680c"
+            url: "https://github.com/jaemyeong/NMapsMap-SPM/releases/download/3.14.2/NMapsMap.xcframework.zip",
+            checksum: "ac07d8bd04c259448a50a6f951f1b442367436782ae0dfc6e8d5ec3b5208dbc0"
         )
     ]
 )
